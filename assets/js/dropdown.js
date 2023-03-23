dda71a9618316e569d7a308ba12b0c72c12251d0
Voi <wojtek.wacek.dola@gmail.com>
2023-03-23 17:27:34 +1100

    Added and eddited few comments.

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,4 +1,4 @@
-/*!
+/* Sourced at https://codepen.io/yassinevic/pen/eXjqjb
  * Materialize v1.0.0 (http://materializecss.com)
  * Copyright 2014-2017 Materialize
  * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
