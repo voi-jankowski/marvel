5b81c95949c7f69e6e2f68bd4c679176063cfaf5
Voi <wojtek.wacek.dola@gmail.com>
2023-03-24 13:17:43 +1100

    Assigned #movie-title to correct elements in HTML to give the functions correct search value.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 var searchButton = $("button");
-var expression = $("#movie-title");
-var resultContainer = $(".container")
-
+var expression = $("#movie-title").val();
+var resultContainer = $(".container");
+console.log(expression);
 const apiKey = "k_42i4oflg";
 
 function getApi() {
-  expression = "spiderman"; //sample search keyword only
-  var requestUrl = "https://imdb-api.com/en/API/SearchMovie/" + apiKey + "/" + expression;
+  //   expression = "spiderman"; //sample search keyword only
+  var requestUrl =
+    "https://imdb-api.com/en/API/SearchMovie/" + apiKey + "/" + expression;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -14,8 +15,13 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
-      
-      var requestUrl2 = "https://imdb-api.com/en/API/Title/" + apiKey + "/" + data.results[0].id + "/Trailer";
+
+      var requestUrl2 =
+        "https://imdb-api.com/en/API/Title/" +
+        apiKey +
+        "/" +
+        data.results[0].id +
+        "/Trailer";
       // console.log(requestUrl2);
 
       fetch(requestUrl2)
@@ -23,7 +29,7 @@ function getApi() {
           return response.json();
         })
         .then(function (data) {
-          console.log(data)
+          console.log(data);
           console.log(data.title);
           console.log(data.year);
           console.log(data.contentRating);
@@ -37,26 +43,25 @@ function getApi() {
           var newSearch = {
             title: data.title,
             image: data.image,
-          }
+          };
 
-          var savedSearch = JSON.parse(localStorage.getItem("savedSearch")) || [];
+          var savedSearch =
+            JSON.parse(localStorage.getItem("savedSearch")) || [];
           savedSearch.push(newSearch);
           localStorage.setItem("savedSearch", JSON.stringify(savedSearch));
-        })
-
+        });
     });
 }
 
 // Persist saved events during page refresh
 window.onload = function () {
-  var savedSearch = JSON.parse(window.localStorage.getItem('savedSearch'));
+  var savedSearch = JSON.parse(window.localStorage.getItem("savedSearch"));
   if (savedSearch !== null) {
     for (var i = 0; i < savedSearch.length; i++) {
       var title = savedSearch[i].title;
       var image = savedSearch[i].image;
     }
   }
-}
+};
 
-
-document.addEventListener('click', getApi);
+document.addEventListener("click", getApi);
